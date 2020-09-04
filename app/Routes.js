3569a62cf67348a6cb25e7a03166c17496cc25df
t0g00py <t0g00py@wal-mart.com>
2020-09-04 14:20:42 +0100

    start working on app again

--- app/Routes.js
+++ app/Routes.js
@@ -4,26 +4,19 @@ import About from './screens/About'
 import Contact from './screens/Contact'
 import SideMenu from './screens/SideMenu'
 import {createStackNavigator} from "@react-navigation/stack";
-import {Text, View} from 'react-native'
 import {NavigationContainer} from "@react-navigation/native";
 import DrawerNavigator from "@react-navigation/drawer/src/navigators/createDrawerNavigator";
 
-
 const Stack = createStackNavigator()
-
-function DetailsScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
-        </View>
-    );
-}
 
 function AppStack(){
     return (
-        <Stack.Navigator initialRouteName="Home" >
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="About" component={About} />
+        <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{headerTransparent: true, title: false}}
+        >
+            <Stack.Screen name="Welcome" component={Home} />
+            <Stack.Screen name="About Me" component={About} />
             <Stack.Screen name="Contact" component={Contact} />
         </Stack.Navigator>
     )
@@ -34,7 +27,7 @@ const Drawer = DrawerNavigator();
 function AppDrawer() {
     return (
         <NavigationContainer>
-            <Drawer.Navigator initialRouteName="AppStack" drawerContent={SideMenu} >
+            <Drawer.Navigator initialRouteName="AppStack" drawerContent={SideMenu}>
                 <Drawer.Screen name="AppStack" component={AppStack} />
             </Drawer.Navigator>
         </NavigationContainer>
